42c68dcb11a37ac503e6e86b30eb43733daa6134
soufianbouazzaoui03 <soufianbouazzaoui96@gmail.com>
2024-03-27 10:59:41 +0000

    fd

--- pipe/ft_pipe.c
+++ pipe/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: soel-bou <soel-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 01:20:36 by soel-bou          #+#    #+#             */
-/*   Updated: 2024/03/27 08:08:59 by soel-bou         ###   ########.fr       */
+/*   Updated: 2024/03/27 10:57:09 by soel-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,26 +75,54 @@ void    init_fds(t_cmd **cmds)
 {
     t_cmd *head;
     token_node *tmp;
+    int old_fd_in;
+    int old_fd_out;
 
     head = *cmds;
-    while (cmds)
+    while (head)
     {
         tmp = head->redir;
         while(tmp)
         {
-            if(tmp->type == 0)
+            old_fd_out = head->outfile;
+            old_fd_in = head->infile;
+            if(tmp->type == REDIRECT_OUT)
+            {
+                head->outfile = open(tmp->value, O_RDWR | O_CREAT | O_TRUNC, 0777);
+                if(old_fd_out != head->outfile)
+                {
+                    close(old_fd_out);
+                    old_fd_out = head->outfile;
+                }
+                if(head->outfile < 0)
+                    perror("fd_out");
+            }
+            else if(tmp->type == REDIRECT_APPEND)
+            {
+                head->outfile = open(tmp->value, O_RDWR | O_CREAT | O_APPEND, 0777);
+                if(old_fd_out != head->outfile)
+                {
+                    close(old_fd_out);
+                    old_fd_out = head->outfile;
+                }
+                if(head->outfile < 0)
+                    perror("fd_app");
+            }
+            else if(tmp->type == REDIRECT_IN)
+            {
+                head->infile = open(tmp->value, O_RDWR, 0777);
+                if(old_fd_in != head->infile)
+                {
+                    close(old_fd_in);
+                    old_fd_in = head->infile;
+                }
+                if(head->infile < 0)
+                    perror("fd_in");
+            }
+            tmp = tmp->next;
         }
-    }
-}
-
-int get_fd_out(token_node *node)
-{
-    int fd;
-    token_node *tmp;
-
-    while(tmp)
-    {
-        
+            
+        head = head->next;
     }
 }
 
@@ -155,90 +183,82 @@ void    pipe_line(t_cmd *cmd, t_expand *env_lst, char *env[])
         i++;
     }
 }
-
-int main(int argc, char *argv[], char *env[])
+#include <signal.h>
+int main(int argc, char *argv[], char **env)
 {
     char *cmd[] = {"/bin/echo","-la", NULL};
-    char *cmd2[] = {"/bin/cat", NULL};
-    char *cmd3[] = {"/bin/ls","-lah", NULL};
-    char *cmd4[] = {"/bin/cat", NULL};
-    int fd = open("test", O_RDWR | O_CREAT | O_TRUNC, 0777);
-    t_cmd head;
-    t_cmd head2;
-    t_cmd head3;
-    t_cmd head4;
-    head.args = cmd;
-    head.infile = 0;
-    head.outfile = fd;
-    head.islast = false;
-    head.isfirst = true;
-    head.next = &head4;
-    
-    // head2.args = cmd2;
-    // head2.infile = 0;
-    // head2.outfile = 1;
-    // head2.islast = false;
-    // head2.isfirst = false;
-    // head2.next = &head3;
-    
-    
-    // head3.args = cmd3;
-    // head3.infile = 0;
-    // head3.outfile = 1;
-    // head3.islast = false;
-    // head3.isfirst = false;
-    // head3.next = &head4;
+    token_node *token = (token_node*)malloc(sizeof(token_node));
+    token_node *token2 = (token_node*)malloc(sizeof(token_node));
+    token_node *token3 = (token_node*)malloc(sizeof(token_node));
+    token->type = REDIRECT_APPEND;
+    token->value = "file";
+    token->next = token2;
+    token2->type = REDIRECT_OUT;
+    token2->value = "file3";
+    token2->next = token3;
+    token3->type = REDIRECT_OUT;
+    token3->value = "file4";
+    token3->next = NULL;
+    t_cmd *head = (t_cmd *)malloc(sizeof(t_cmd));
+    head->args = cmd;
+    head->isfirst = true;
+    head->islast = true;
+    head->next = NULL;
+    head->redir = token;
+    init_fds(&head);
+    printf("%d\n", head->outfile);
+    printf("%d", head->infile);
+    pid_t pid = getpid();
 
-    head4.args = cmd4;
-    head4.infile = fd;
-    head4.outfile = 1;
-    head4.islast = true;
-    head4.isfirst = false;
-    head4.next = NULL;
-    pipe_line(&head, NULL, env);
+    // Create a buffer to hold the command
+    char command[100];
+    
+    // Construct the command with the PID
+    snprintf(command, sizeof(command), "lsof -p %d", (int)pid);
+
+    // Execute the command
+    system(command);
     
 }
-// void    run_multi_pipe(char *cmd1[], char *cmd2[], int fd[], char *env[])
+// int main(int argc, char *argv[], char *env[])
 // {
-//     int pid1;
-//     int pid2;
+//     char *cmd[] = {"/bin/echo","-la", NULL};
+//     char *cmd2[] = {"/bin/cat", NULL};
+//     char *cmd3[] = {"/bin/ls","-lah", NULL};
+//     char *cmd4[] = {"/bin/cat", NULL};
+//     int fd = open("test", O_RDWR | O_CREAT | O_TRUNC, 0777);
+//     t_cmd head;
+//     t_cmd head2;
+//     t_cmd head3;
+//     t_cmd head4;
+//     head.args = cmd;
+//     head.infile = 0;
+//     head.outfile = fd;
+//     head.islast = false;
+//     head.isfirst = true;
+//     head.next = &head4;
+    
+//     // head2.args = cmd2;
+//     // head2.infile = 0;
+//     // head2.outfile = 1;
+//     // head2.islast = false;
+//     // head2.isfirst = false;
+//     // head2.next = &head3;
+    
+    
+//     // head3.args = cmd3;
+//     // head3.infile = 0;
+//     // head3.outfile = 1;
+//     // head3.islast = false;
+//     // head3.isfirst = false;
+//     // head3.next = &head4;
 
-//     if (pipe(fd) < 0)
-//         perror("pipe");
-//     if ((pid1 = frok()) < 0 || (pid2 =frok()) < 0)
-//         perror("fork");
-//     if (pid1 == 0)
-//     {
-//         close(fd[1]);
-//         dup2(1, fd[1]);
-//         close(fd[1]);
-//         execve(cmd1[0], cmd1, env);
-//         perror(cmd1[0]);
-//     }
-//     if (pid2 == 0)
-//     {
-//         close(fd[1]);
-//         dup2(1, fd[1]);
-//         close(fd[1]);
-//         execve(cmd2[0], cmd2, env);
-//         perror(cmd2[0]);
-//     }
-// }
-
-// void    pipe_it(t_cmd *cmd, char *env[])
-// {
-//     int fd[2];
-//     int pid;
-
-//     while(cmd)
-//     {
-//         pipe(fd);
-//         pid = fork();
-//         if(pid == 0)
-//         {
-//             if(cmd->next)
-//                 dup2(fd[1], 1);
-//             close(fd[1]);
-//         }
-//     }
+//     head4.args = cmd4;
+//     head4.infile = fd;
+//     head4.outfile = 1;
+//     head4.islast = true;
+//     head4.isfirst = false;
+//     head4.next = NULL;
+//     pipe_line(&head, NULL, env);
+    
 // }
